e62a0df415caaae58b295c067d7f2ad4f2e8e79c
Noé Fontana <noe.fontana.pro@gmail.com>
2025-10-04 18:00:06 -0400

    refactor(augmentation): Reorder

--- src/segpaste/augmentation/lsj.py
+++ src/segpaste/augmentation/lsj.py
@@ -16,49 +16,6 @@ from torchvision.transforms.v2 import functional as F
 from segpaste.types.data_structures import PaddingMask
 
 
-class RandomResize(Transform):  # type: ignore[misc]
-    def __init__(
-        self,
-        min_scale: float,
-        max_scale: float,
-        target_height: int,
-        target_width: int,
-    ) -> None:
-        """Randomly resize the input image while preserving aspect ratio.
-
-        The final size is obtained by scaling the target height and width with a random
-        factor.
-        """
-
-        super().__init__()
-        self.min_scale = min_scale
-        self.max_scale = max_scale
-        self.target_height = target_height
-        self.target_width = target_width
-
-    def make_params(
-        self,
-        flat_inputs: list[tv_tensors.TVTensor | torch.Tensor],  # noqa: ARG002
-    ) -> dict[str, float]:
-        scale = random.uniform(self.min_scale, self.max_scale)
-        return {"scale": scale}
-
-    def transform(
-        self, inpt: tv_tensors.TVTensor | torch.Tensor, params: dict[str, float]
-    ) -> Any:
-        h, w = F.get_size(inpt)
-        scale: float = params["scale"]
-
-        target_scale_h, target_scale_w = (
-            self.target_height * scale,
-            self.target_width * scale,
-        )
-        output_scale = min(target_scale_h / h, target_scale_w / w)
-
-        new_h, new_w = round(h * output_scale), round(w * output_scale)
-        return self._call_kernel(F.resize, inpt, [new_h, new_w])
-
-
 class FixedSizeCrop(Transform):  # type: ignore[misc]
     def __init__(
         self,
@@ -115,6 +72,49 @@ class FixedSizeCrop(Transform):  # type: ignore[misc]
                 cropped[..., :, w:] = self.seg_pad_value
 
         return cropped
+
+
+class RandomResize(Transform):  # type: ignore[misc]
+    def __init__(
+        self,
+        min_scale: float,
+        max_scale: float,
+        target_height: int,
+        target_width: int,
+    ) -> None:
+        """Randomly resize the input image while preserving aspect ratio.
+
+        The final size is obtained by scaling the target height and width with a random
+        factor.
+        """
+
+        super().__init__()
+        self.min_scale = min_scale
+        self.max_scale = max_scale
+        self.target_height = target_height
+        self.target_width = target_width
+
+    def make_params(
+        self,
+        flat_inputs: list[tv_tensors.TVTensor | torch.Tensor],  # noqa: ARG002
+    ) -> dict[str, float]:
+        scale = random.uniform(self.min_scale, self.max_scale)
+        return {"scale": scale}
+
+    def transform(
+        self, inpt: tv_tensors.TVTensor | torch.Tensor, params: dict[str, float]
+    ) -> Any:
+        h, w = F.get_size(inpt)
+        scale: float = params["scale"]
+
+        target_scale_h, target_scale_w = (
+            self.target_height * scale,
+            self.target_width * scale,
+        )
+        output_scale = min(target_scale_h / h, target_scale_w / w)
+
+        new_h, new_w = round(h * output_scale), round(w * output_scale)
+        return self._call_kernel(F.resize, inpt, [new_h, new_w])
 
 
 class SanitizeBoundingBoxes(tv_SanitizeBoundingBoxes):  # type: ignore[misc]
