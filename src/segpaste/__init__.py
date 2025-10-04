cf2df600ec9515e21df0896c568050e577cfa18e
Noé Fontana <noe.fontana.pro@gmail.com>
2025-10-04 18:00:46 -0400

    fix(init): Update imports and __all__ to include FixedSizeCrop, RandomResize, and PaddingMask

--- src/segpaste/__init__.py
+++ src/segpaste/__init__.py
@@ -1,22 +1,32 @@
 # Import main functionality here
+import logging
+
 try:
     import faster_coco_eval as _faster_coco_eval
 
     _faster_coco_eval.init_as_pycocotools()
 except ImportError:
+    logging.getLogger(__file__).warning("faster_coco_eval not found.")
     pass
 from segpaste.augmentation import (
-    CopyPasteAugmentation,
     CopyPasteCollator,
     CopyPasteTransform,
+    FixedSizeCrop,
+    RandomResize,
+    SanitizeBoundingBoxes,
+    make_large_scale_jittering,
 )
 from segpaste.config import CopyPasteConfig
-from segpaste.types import DetectionTarget
+from segpaste.types import DetectionTarget, PaddingMask
 
 __all__ = [
-    "CopyPasteAugmentation",
+    "PaddingMask",
     "CopyPasteConfig",
     "DetectionTarget",
     "CopyPasteTransform",
     "CopyPasteCollator",
+    "FixedSizeCrop",
+    "RandomResize",
+    "SanitizeBoundingBoxes",
+    "make_large_scale_jittering",
 ]
